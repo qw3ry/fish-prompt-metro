b705b4a23faab84841020e9bea8403337f268a23
Mark Lowne <marklowne@gmail.com>
2020-03-03 16:10:26 +0100

    remove `fish_greeting` as it doesn't belong here

--- fish_greeting.fish
+++
@@ -1,7 +0,0 @@
-function fish_greeting
-    set_color $fish_color_autosuggestion
-    uname -nmsr
-    command -qs uptime; and command uptime
-    echo (id -un)'@'(hostname -s)
-    set_color normal
-end
